4dfb5a7f0e6dd90ade3a01ecf66c575f53b1d237
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.fios-router.home>
2021-05-06 17:31:36 -0400

    state lecture

--- classComponents/class-component-menu/src/App.js
+++ classComponents/class-component-menu/src/App.js
@@ -6,9 +6,14 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Everybody, welcome to the MERN (grass fed, gluten free, free range) restruarant</h1>
-      <MenuItem name= {"Calamari"} price= {12.50} desc = {"Its grass fed tho"}/>
-      <MenuItem name= {"Basil Salmon Salad"} price= {200.00} desc = {"The basil comes from the deep trenches of Java, its grass fed, and its data type is FreeRange"}></MenuItem>
-      <MenuItem name= {"Nepali Treats from my fam"} price= {12.00} desc = {"rob be alvin an the chipmunks in week 3 after eating this"}/>
+      <MenuItem name= {"Calamari"} price= {12.50} desc = {"Its grass fed tho"} likes = {0}>
+          <p>*Gluten Free</p>
+          <p>Contains Nuts</p>
+
+      </MenuItem>
+
+      <MenuItem name= {"Basil Salmon Salad"} price= {200.00} desc = {"The basil comes from the deep trenches of Java, its grass fed, and its data type is FreeRange"} likes = {0}></MenuItem>
+      <MenuItem name= {"Nepali Treats from my fam"} price= {12.00} desc = {"rob be alvin an the chipmunks in week 3 after eating this"} likes = {10}/>
     </div>
   );
 }
--- classComponents/class-component-menu/src/components/MenuItem.jsx
+++ classComponents/class-component-menu/src/components/MenuItem.jsx
@@ -1,16 +1,91 @@
 import React, { Component } from 'react';
 
 
+// class LightSwitch extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             position: "On"
+//         };
+//     }
+    
+//     render() {
+//         return (
+//             <fieldset>
+//                 <p>The light is currently { this.state.position }</p>
+//                 <button>Flip Switch</button>
+//             </fieldset>
+//         );
+//     }
+// }
+                
+// export default LightSwitch;
+
 class MenuItem extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            likesState: props.likes,
+            dislike: 0
+        }
+    }
+    
+
     render() {
         //you can do regular js code here inside the render(){} function body but before the return
         console.log("**********",this.props)
-        const {name, price, desc} = this.props
-        return<div>
-            <h1>Name: {name} </h1>
-            <h3>Price: {price}</h3>
-            <p>Description: {desc}</p>
-        </div>
+        let {name, price, desc, children, likes} = this.props
+
+        const likeItem = ()=>{
+            // this.state.likesState+=1
+            this.setState({likesState:this.state.likesState+1})
+            console.log("clicked like button, this is the likes variable -->", likes)
+        }
+        const dislikeItem = ()=>{
+            // this.state.likesState+=1
+            this.setState({dislike:this.state.dislike+1})
+            console.log("clicked like button, this is the likes variable -->", likes)
+        }
+        
+
+
+        // <div class="card">
+        //     <div class="card-body">
+        //         <h4 class="card-title">Card title</h4>
+        //         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        //         <p class="card-text">
+        //         Some quick example text to build on the card title
+        //         and make up the bulk of the card's content.
+        //         </p>
+        //         <a href="#!" class="card-link">Card link</a>
+        //         <a href="#!" class="card-link">Another link</a>
+        //     </div>
+        // </div>
+
+        return(
+            <div className="card">
+                <div className="card-body">
+                
+                    <h1 className= "card-title">Name: {name} </h1>
+                    <div className= "card-subtitle mb-2 text-muted">
+                        {children}
+                    </div>
+                    <div className="card-text">
+                        <h3>Price: {price}</h3>
+                        <p>Description: {desc}</p>
+                        {/* <button onClick={ ()=> alert("This button has been clicked!") }>Click Me</button> */}
+
+                        <p><button className = "btn btn-success" onClick={likeItem}>Like</button> Likes: {this.state.likesState} </p>
+                        <p><button className = "btn btn-danger" onClick={dislikeItem}>Dislike</button> Dislikes: {this.state.dislike} </p>
+
+                    </div>
+
+
+                    
+                </div>
+            </div>
+        )
             
        
     }
