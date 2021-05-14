9dcea62f0cb973d6eca8c2d02ee03d2cdbfceeef
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.fios-router.home>
2021-05-14 12:52:41 -0400

    mongo db commands intro lecture

---
+++ algos/quickBrownFox.js
@@ -0,0 +1,47 @@
+let words = [
+    ["quick", "lazy"],
+    ["brown", "red", "grey"],
+    ["fox", "dog"]
+
+]
+
+words[0][1]
+
+
+// ["quick brown fox", "quick brown dog", "quick red fox" etc;]
+
+
+
+function allcombs(words){
+    let result =[];
+    function helper(comb, i){
+        if(i==words.length){ //if we have a word from each of the sub arrays
+            result.push(comb)
+        }
+        else{ //if we are still generating combination words --> comb looks like "", or "quick" or "quick brown"
+            for(let j = 0; j< words[i].length; j++){
+                helper(comb+ words[i][j] + " ", i+1)
+            }
+
+        }
+    }
+    helper("", 0)
+    return result
+}
+
+
+console.log(allcombs(words))//["quick brown fox", "quick brown dog", "quick red fox" etc;]
+
+
+
+
+
+
+// function decode(str){
+
+// }
+
+// decode("b3c4d3y2") //"bbbccccdddyy"
+
+
+
